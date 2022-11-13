aa934d6384447be1ed38795f1f7ce1dccf4615d4
mohamedchaali2020 <mohamed.chaali@esprit.tn>
2022-11-13 22:21:33 +0100

    modified:   src/main/java/tn/esprit/rh/achat/services/FactureServiceImpl.java 	modified:   src/main/java/tn/esprit/rh/achat/services/FournisseurServiceImpl.java 	modified:   src/main/java/tn/esprit/rh/achat/services/ProduitServiceImpl.java 	modified:   src/main/java/tn/esprit/rh/achat/services/StockServiceImpl.java

--- src/main/java/tn/esprit/rh/achat/services/StockServiceImpl.java
+++ src/main/java/tn/esprit/rh/achat/services/StockServiceImpl.java
@@ -1,4 +1,4 @@
-/*package tn.esprit.rh.achat.services;
+package tn.esprit.rh.achat.services;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -84,4 +84,3 @@ public class StockServiceImpl implements IStockService {
 	}
 
 }
-*/
