904c73ccd46398953d1bfb4ca74e9ccf1ca66625
mohamedchaali2020 <mohamed.chaali@esprit.tn>
2022-11-14 08:17:23 +0100

    modified:   Dockerfile 	modified:   src/main/java/tn/esprit/rh/achat/controllers/CategorieProduitController.java 	modified:   src/main/java/tn/esprit/rh/achat/controllers/FactureRestController.java 	modified:   src/main/java/tn/esprit/rh/achat/controllers/FournisseurRestController.java 	modified:   src/main/java/tn/esprit/rh/achat/controllers/OperateurController.java 	modified:   src/main/java/tn/esprit/rh/achat/controllers/ProduitRestController.java 	modified:   src/main/java/tn/esprit/rh/achat/controllers/ReglementRestController.java 	modified:   src/main/java/tn/esprit/rh/achat/controllers/SecteurActiviteController.java 	modified:   src/main/java/tn/esprit/rh/achat/controllers/StockRestController.java 	deleted:    src/main/java/tn/esprit/rh/achat/services/FactureServiceImpl.java

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,6 @@ Run mvn insatall
 
 FROM openjdk:11.0
 WORKDIR /app
-COPY --from-build /app/target/tpmagasin.jar /app/
+COPY -from-build /app/target/tpmagasin.jar /app/
 EXPOSE 9090
 CMD [ "java","jar","tpmagasin.jar" ]
--- src/main/java/tn/esprit/rh/achat/controllers/ReglementRestController.java
+++ src/main/java/tn/esprit/rh/achat/controllers/ReglementRestController.java
@@ -1,20 +1,29 @@
 package tn.esprit.rh.achat.controllers;
-import io.swagger.annotations.Api;
+import java.util.Date;
+import java.util.List;
+
+import tn.esprit.rh.achat.entities.Facture;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
+
 import tn.esprit.rh.achat.entities.Reglement;
 import tn.esprit.rh.achat.services.IReglementService;
-import java.util.Date;
-import java.util.List;
+
+import io.swagger.annotations.Api;
+
 @RestController
 @Api(tags = "Gestion des reglements")
 @RequestMapping("/reglement")
 @CrossOrigin("*")
 public class ReglementRestController {
-	@Autowired
+
+    @Autowired
     IReglementService reglementService;
-	@PostMapping("/add-reglement")
+
+
+    // http://localhost:8089/SpringMVC/reglement/add-reglement
+    @PostMapping("/add-reglement")
     @ResponseBody
     public Reglement addReglement(@RequestBody Reglement r) {
         Reglement reglement = reglementService.addReglement(r);
@@ -26,16 +35,22 @@ public class ReglementRestController {
         List<Reglement> list = reglementService.retrieveAllReglements();
         return list;
     }
+
+    // http://localhost:8089/SpringMVC/reglement/retrieve-reglement/8
     @GetMapping("/retrieve-reglement/{reglement-id}")
     @ResponseBody
     public Reglement retrieveReglement(@PathVariable("reglement-id") Long reglementId) {
         return reglementService.retrieveReglement(reglementId);
     }
+
+    // http://localhost:8089/SpringMVC/reglement/retrieveReglementByFacture/8
     @GetMapping("/retrieveReglementByFacture/{facture-id}")
     @ResponseBody
     public List<Reglement> retrieveReglementByFacture(@PathVariable("facture-id") Long factureId) {
         return reglementService.retrieveReglementByFacture(factureId);
     }
+
+    // http://localhost:8089/SpringMVC/reglement/getChiffreAffaireEntreDeuxDate/{startDate}/{endDate}
     @GetMapping(value = "/getChiffreAffaireEntreDeuxDate/{startDate}/{endDate}")
     public float getChiffreAffaireEntreDeuxDate(
             @PathVariable(name = "startDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date startDate,
